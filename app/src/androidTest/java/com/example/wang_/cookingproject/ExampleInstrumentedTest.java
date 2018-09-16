ac7bee8aca413452d61bd41ac60ae2bd2db067fa
wang- <1715088795@qq.com>
2018-09-16 14:33:33 -0500

    Added UI test

--- app/src/androidTest/java/com/example/wang_/cookingproject/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/wang_/cookingproject/ExampleInstrumentedTest.java
@@ -2,25 +2,39 @@ package com.example.wang_.cookingproject;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.example.wang_.cookingproject.view.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.wang_.cookingproject", appContext.getPackageName());
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void testEditText(){
+
+       onView(withId(R.id.editText_dish)).perform(typeText("Dish"));
+       onView(withId(R.id.editText_dish)).check(matches(withText("Dish")));
     }
+
+
 }
